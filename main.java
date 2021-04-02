523b3aeb8bb19d6bf53ffa3fc398b3a13162e71a
Abdessamad Hamzaoui <58146619+abdessamadhamzaoui@users.noreply.github.com>
2021-04-02 10:49:23 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class main(){
   public static void main(String[] args) {
-        System.out.printlni("Job works perfectly!"); }
+        System.out.printlni("Everything functions well!"); }
   
 }
